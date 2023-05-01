37fea7e8db61a583c0ee9b0d18a54e3d9bb138e8
Jean-Francois/Zermelo <jean-francois.rocher@epfl.ch>
2023-05-01 12:27:41 +0200

    add an exit button

--- app/src/main/java/com/github/zermelo101/screenrecorderapp/RecordedFile.kt
+++ app/src/main/java/com/github/zermelo101/screenrecorderapp/RecordedFile.kt
@@ -1,18 +1,20 @@
 package com.github.zermelo101.screenrecorderapp
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
@@ -28,6 +30,7 @@ import java.net.Socket
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.TimeUnit
 
 
 class RecordedFile : ComponentActivity() {
@@ -104,6 +107,19 @@ class RecordedFile : ComponentActivity() {
          */
 
     }
+    @Composable
+    fun ExitButton() {
+        Box(
+            modifier = Modifier
+                .wrapContentSize()
+                .clickable(onClick = {
+                    val x = Intent(this,MainActivity::class.java)
+        startActivity(x)}),
+            contentAlignment = Alignment.TopEnd
+        ) {
+
+        }
+    }
 
     @Composable
     fun SetUpScreenSize(){
@@ -123,17 +139,28 @@ class RecordedFile : ComponentActivity() {
 
         var touchX by remember { mutableStateOf(0f) }
         var touchY by remember { mutableStateOf(0f) }
+        val context = LocalContext.current
 
-
+        ExitButton()
         Column {
-            Text(text = "Touch X: $touchX")
-            Text(text = "Touch Y: $touchY")
+            Text(
+                text = "EXIT",
+                color = Color.Black,
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .clickable {  val x = Intent(context,MainActivity::class.java)
+                        startActivity(x) }
+
+            )
+            Text(text = "Touch X: $touchX Touch Y: $touchY")
+
+
 
             Box(
                 modifier = Modifier
                     .fillMaxSize()
                     .pointerInteropFilter {
-                        list.add(Pos(it.x,it.y, System.nanoTime() - startTime))
+                        list.add(Pos(it.x, it.y, (System.nanoTime() - startTime)))
                         touchX = it.x
                         touchY = it.y
                         true
